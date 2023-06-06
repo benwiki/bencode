0b72e834049a5f7b33171b3f958479fa368faa8d
Benke Hargitai <hargitai.benjoe@gmail.com>
2023-06-06 16:33:14 +0200

    Updated file structure

---
+++ in_progress/flutter/verneigung/lib/amount_changer.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AmountChanger extends StatelessWidget {
+  final Function(int) changeAmountBy;
+  final Function getAmount;
+  final String title;
+  final String? unit;
+  final List<List>? amountChangerValues;
+  final bool breakAmountChangers;
+
+  final normalTextStyle =
+      const TextStyle(fontSize: 20, color: Color.fromARGB(255, 253, 207, 71));
+  final amountTextStyle = const TextStyle(fontSize: 40, color: Colors.white);
+
+  const AmountChanger(
+      {super.key,
+      required this.changeAmountBy,
+      required this.getAmount,
+      this.title = "Title",
+      this.unit,
+      this.amountChangerValues,
+      this.breakAmountChangers = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(title, style: normalTextStyle),
+        const SizedBox(width: 20),
+        Text(getAmount().toString(), style: amountTextStyle),
+        if (unit != null) ...[
+          const SizedBox(width: 20),
+          Text(unit ?? '', style: normalTextStyle)
+        ]
+      ]),
+      const SizedBox(height: 10),
+      buildButtonMatrix()
+    ]);
+  }
+
+  Widget buildButtonMatrix() {
+    return Column(children: [
+      for (int i = 0; i < (amountChangerValues?.length ?? 0); ++i) ...[
+        if (i > 0) const SizedBox(height: 10),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          for (int j = 0; j < (amountChangerValues?[i].length ?? 0); ++j) ...[
+            if (j > 0) const SizedBox(width: 10),
+            buildAmountChangeButton(amountChangerValues?[i][j] ?? 0)
+          ]
+        ])
+      ]
+    ]);
+  }
+
+  Widget buildAmountChangeButton(int changeValue) {
+    return SizedBox(
+        width: 100,
+        height: 45,
+        child: ElevatedButton(
+            onPressed: () => changeAmountBy(changeValue),
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))),
+            child: Text('${changeValue > 0 ? '+' : ''}$changeValue',
+                style: const TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.bold))));
+  }
+}
