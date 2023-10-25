fc733b81da9e4ae2ed7211d60a8a19833aeac490
Benkex <hargitai.benjoe@gmail.com>
2023-10-26 00:37:35 +0300

    Update

--- @ctive/haskell/tuni_exercises/week5.hs
+++ @ctive/haskell/tuni_exercises/week5.hs
@@ -1,9 +1,37 @@
+
 gap :: (Char, Char) -> Int -> String -> Int
 
-gap _ gaplength (_:rest)
-    | (gaplength < 0 || length rest <= gaplength)
+gap _ gaplen (_:rest)
+    | (gaplen < 0 || length rest <= gaplen)
  = 0
 
 gap (c1, c2) gaplen (first:rest) =
     ( if first == c1 && rest !! gaplen == c2 then 1 else 0 )
     + gap (c1, c2) gaplen rest
+
+-- Alternative: --
+-- gap (c1, c2) gaplen (first:rest)
+--     | (gaplen < 0 || length rest <= gaplen)
+--  = 0
+--     | (first == c1 && rest !! gaplen == c2)
+--  = 1 + gap (c1, c2) gaplen rest
+--     | otherwise
+--  = gap (c1, c2) gaplen rest
+
+distance1 :: String -> String -> Float
+
+distance1 "" "" = 0
+distance1 s1 s2 =
+   fromIntegral (sum [(if e1 `notElem` s2 then 1 else 0) | e1 <- s1]
+                  + sum [(if e2 `notElem` s1 then 1 else 0) | e2 <- s2])
+                / fromIntegral (length s1 + length s2)
+-- Alternative: --
+-- distance1 s1 s2 = fromIntegral (length (filter (`notElem` s2) s1) + length (filter (`notElem` s1) s2)) / fromIntegral (length s1 + length s2)    
+
+distance2 :: String -> String -> Float
+
+distance2 "" "" = 0
+distance2 s1 s2 =
+   fromIntegral (sum [(if e1 `notElem` ['0'..'9'] then 1 else 0) | e1 <- s1]
+                   + sum [(if e2 `notElem` ['0'..'9'] then 1 else 0) | e2 <- s2])
+                / fromIntegral (length s1 + length s2)
--- @ctive/haskell/tuni_exercises/week7.hs
+++ @ctive/haskell/tuni_exercises/week7.hs
@@ -1,2 +1,33 @@
--- clusters :: (String -> String -> Float) -> Float -> [String] -> [[String]]
-finterEven = filter even
+
+clusters :: (String -> String -> Float) -> Float -> [String] -> [[String]]
+clusters f d ss = map (\s -> filter (\x -> f x s <= d) ss) ss
+
+
+{- Task:
+Write a function commonSubstring :: String -> String -> String that, given two strings s1 and s2, computes a common “substring” of s1 and s2 as follows. The function finds the earliest common character c (a character closest to the head of either s1 or s2, and appearing in both sequences). The function removes c and all the characters before it in both strings, puts c in the output string, and continues.
+If there are two candidates for the earliest common character, pick the one from s1.
+For example:
+commonSubstring "XabcdefgY" "abcdefgXY" produces "XY"
+commonSubstring "abcdefgXY" "XabcdefgY" produces "abcdefgY"
+Please note that the result is not what is normally meant by substring.
+-}
+commonSubstring :: String -> String -> String
+commonSubstring _ "" = ""
+commonSubstring "" _ = ""
+commonSubstring s1 s2
+    | head s1 `elem` s2
+ = head s1 : commonSubstring (tail s1) (tail (dropWhile (/= head s1) s2))
+    | head s2 `elem` s1
+ = head s2 : commonSubstring (tail (dropWhile (/= head s2) s1)) (tail s2)
+    | otherwise
+ = commonSubstring (tail s1) (tail s2)
+
+
+-- Alternative:
+------------------------------------------------------------------------
+-- commonSubstring :: String -> String -> String
+-- commonSubstring _ "" = ""
+-- commonSubstring "" _ = ""
+-- commonSubstring (x:xs) s2
+--     | x `elem` s2 = x : commonSubstring xs (tail (dropWhile (/= x) s2))
+--     | otherwise = commonSubstring xs s2
